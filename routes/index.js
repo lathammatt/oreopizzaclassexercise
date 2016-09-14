7171a9bce1fc26358b607983c87cb7b2581a44ab
Matt Latham <latham75@gmail.com>
2016-09-14 11:01:27 -0500

    changed var in routes

--- routes/index.js
+++ routes/index.js
@@ -8,33 +8,33 @@ const Order = require('../models/orders')
 
 // module.exports = function(app) {
 
-	router.get("/", (req, res) => {
-		res.render('index')
-	})
-	router.get("/about", (req, res) => {
-		res.render('about', {page: 'About'})
-	})
-	router.get("/contact", (req, res) => {
-		res.render('contact', {page: 'Contact'})
-	})
-	router.post("/contact", (req, res) => {
-		const msg = new Contact(req.body)
-		msg.save()
-			.then(() => res.redirect('/'))
-			.catch(() => res.send('bad'))
-	})
-	router.get('/order', (req, res) => {
-		res.render('order', {page:'Order'})
-	})
-	router.post('/order', (req, res) => {
-		const msg = new Order(req.body)
-		msg.save()
-			.then(() => res.redirect('/'))
-			.catch(() => res.send('bad'))
-	})
-	router.get("/404", (req, res) => {
-		res.render('404', {page: '404'})
-	})
+router.get("/", (req, res) => {
+	res.render('index')
+})
+router.get("/about", (req, res) => {
+	res.render('about', {page: 'About'})
+})
+router.get("/contact", (req, res) => {
+	res.render('contact', {page: 'Contact'})
+})
+router.post("/contact", (req, res) => {
+	const msg = new Contact(req.body)
+	msg.save()
+		.then(() => res.redirect('/'))
+		.catch(() => res.send('bad'))
+})
+router.get('/order', (req, res) => {
+	res.render('order', {page:'Order'})
+})
+router.post('/order', (req, res) => {
+	const order = new Order(req.body)
+	order.save()
+		.then(() => res.redirect('/'))
+		.catch(() => res.send('bad'))
+})
+router.get("/404", (req, res) => {
+	res.render('404', {page: '404'})
+})
 
 
 
